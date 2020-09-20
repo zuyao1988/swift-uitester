aad3af488f87019e316e04e536db39f94e1a0c3d
zuyao <tyaolee@liveperson.com>
2020-09-20 21:12:32 +0800

    Initial Commit

---
+++ UiTester/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UiTester
+//
+//  Created by Tzuu Yao Lee on 20/9/20.
+//  Copyright © 2020 Tzuu Yao Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
